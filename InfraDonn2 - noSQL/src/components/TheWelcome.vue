7085ad33eaf5b3e966eef97aa808531ef37b3678
sachaloskov <sacha.loskov@gmail.com>
2025-10-09 11:57:30 +0200

    PouchDB

--- InfraDonn2 - noSQL/src/components/TheWelcome.vue
+++ InfraDonn2 - noSQL/src/components/TheWelcome.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import PouchDB from 'pouchdb';
 
 const counter = ref(0);
 
 const increment = () => {
   counter.value++
 }
+
+function mounted() {
+    this.initDatabase()
+}
+
+function initDatabase() {
+  const db = new PouchDB('http://admin:admin@localhost:5984/test_infradonn2')
+}
+
 </script>
 
 <template>
