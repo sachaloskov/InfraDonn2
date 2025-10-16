f4b9361611cfc17b8d2ceda73ad7a67aae2c1263
sachaloskov <sacha.loskov@gmail.com>
2025-10-16 09:16:32 +0200

    copier-coller welcome-vue + installation events

--- InfraDonn2 - noSQL/src/components/TheWelcome.vue
+++ InfraDonn2 - noSQL/src/components/TheWelcome.vue
@@ -1,25 +1,52 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import PouchDB from 'pouchdb';
+import { onMounted, ref } from 'vue';
+import PouchDB from 'pouchdb'
 
-const counter = ref(0);
-
-const increment = () => {
-  counter.value++
+declare interface Post {
+  post_name: string;
+  post_content: string;
+  attributes: {
+    creation_date: any;
+  };
 }
 
-function mounted() {
-    this.initDatabase()
+// Référence à la base de données
+const storage = ref()
+// Données stockées
+const postsData = ref<Post[]>([])
+
+// Initialisation de la base de données
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données');
+  const db = new PouchDB('http://admin:admin@localhost:5984/database')
+  if (db) {
+    console.log("Connecté à la collection : " + db?.name)
+    storage.value = db
+  } else {
+    console.warn('Echec lors de la connexion à la base de données')
+  }
 }
 
-function initDatabase() {
-  const db = new PouchDB('http://admin:admin@localhost:5984/test_infradonn2')
+// Récupération des données
+const fetchData = () => {
+  // TODO Récupération des données
+  // https://pouchdb.com/api.html#batch_fetch
+  // Regarder l'exemple avec function allDocs
+  // Remplir le tableau postsData avec les données récupérées
 }
+
+onMounted(() => {
+  console.log('=> Composant initialisé');
+  initDatabase()
+  fetchData()
+});
 
 </script>
 
 <template>
-  <h1>Welcome</h1>
-  <p>Counter: {{ counter }}</p>
-  <button @click="increment">Increment</button>
+  <h1>Fetch Data</h1>
+  <article v-for="post in postsData" v-bind:key="(post as any).id">
+    <h2>{{ post.post_name }}</h2>
+    <p>{{ post.post_content }}</p>
+  </article>
 </template>
